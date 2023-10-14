3c492b2e9db60339ec987c4c925e0185ac5f0391
VICTOR <yo@casa.com>
2023-10-14 14:03:28 +0200

    programa funcional, añadidos comentarios para el video

--- src/main/java/org/example/CombinarCorrespondencia.java
+++ src/main/java/org/example/CombinarCorrespondencia.java
@@ -13,25 +13,34 @@ public class CombinarCorrespondencia {
      * Si falta el archivo, se lanza una excepción.
      */
     public static Map<String, Cliente> cargarClientes() throws ArchivosFaltantesException {
+        // Crea un mapa para almacenar los datos de los clientes (clave: código de cliente, valor: objeto Cliente).
         Map<String, Cliente> clientes = new HashMap<>();
         try {
+            // Abre y lee el archivo ListaClientes.csv en la carpeta resources.
             BufferedReader br = new BufferedReader(new FileReader("src/main/resources/ListaClientes.csv"));
             String line;
+            // Lee cada línea del archivo CSV mientras que no este vacia
             while ((line = br.readLine()) != null) {
+                // Divide la línea en partes utilizando el punto y coma como separador.
                 String[] datos = line.split(";");
-
+                // Valor predeterminado de codigo si no hay suficientes datos, en este caso espera 5 elementos.
                 String codigo = "DATOS en csv Insuficientes";
+                // Comprueba si la línea contiene al menos 5 elementos.
                 if (datos.length >= 5) {
+                    // Si hay suficientes datos, asigna los valores a las variables correspondientes.
                     codigo = datos[0];
                     String nombreEmpresa = datos[1];
                     String localidad = datos[2];
                     String correo = datos[3];
                     String nombreResponsable = datos[4];
+                    // Agrega un nuevo objeto Cliente al mapa utilizando el código de cliente como clave.
                     clientes.put(codigo, new Cliente(codigo, nombreEmpresa, localidad, correo, nombreResponsable));
                 } else {
+                    // Si no hay suficientes datos, agrega un objeto Cliente con valores de "ERROR" al mapa.
                     clientes.put(codigo, new Cliente("ERROR", "ERROR", "ERROR", "ERROR", "ERROR"));
                 }
             }
+            // Cierra el archivo después de leerlo.
             br.close();
         } catch (IOException e) {
             throw new ArchivosFaltantesException("Falta el archivo ListaClientes.csv en la carpeta resources");
@@ -52,27 +61,32 @@ public class CombinarCorrespondencia {
             }
             br.close();
         } catch (IOException e) {
+            // Si se produce una excepción IOException, lanza una excepción ArchivosFaltantesException
+            // indicando que falta el archivo ListaClientes.csv en la carpeta resources.
             throw new ArchivosFaltantesException("Falta el archivo template.txt en la carpeta resources");
         }
+        // Devuelve el mapa que contiene los datos de los clientes.
         return contenido.toString();
     }
     /**
-     * Combina la correspondencia para cada cliente y genera archivos de salida en la carpeta "salida".
+     * Combina la template para cada cliente y genera archivos de salida en la carpeta "salida".
      */
     public static void combinarCorrespondencia(Map<String, Cliente> clientes, String plantilla) {
+        // Recorre el mapa de clientes, donde la clave es el código de cliente y el valor es un objeto Cliente.
         for (Map.Entry<String, Cliente> entry : clientes.entrySet()) {
-            String codigo = entry.getKey();
-            Cliente cliente = entry.getValue();
+            String codigo = entry.getKey(); // Obtiene el código de cliente.
+            Cliente cliente = entry.getValue(); // Obtiene el objeto Cliente.
 
+            // Reemplaza los marcadores en la plantilla con los datos del cliente.
             String contenido = plantilla
                     .replace("%%1%%", codigo)
                     .replace("%%2%%", cliente.getNombreEmpresa())
                     .replace("%%3%%", cliente.getLocalidad())
                     .replace("%%4%%", cliente.getCorreo())
                     .replace("%%5%%", cliente.getNombreResponsable());
-
+            // Ponemos el nombre del archivo de salida personalizado para cada cliente.
             String nombreArchivo = "src/main/resources/salida/template-" + codigo + ".txt";
-
+            // Crea un FileWriter para escribir el contenido en el archivo de salida y su contenido.
             try (FileWriter writer = new FileWriter(nombreArchivo)) {
                 writer.write(contenido);
             } catch (IOException e) {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,17 +14,21 @@ import static org.example.CombinarCorrespondencia.*;
 public class Main {
     public static void main(String[] args) {
         try {
+            // Intenta cargar los datos de los clientes y la plantilla.
             Map<String, Cliente> clientes = cargarClientes();
             String plantilla = cargarPlantilla();
 
             if (clientes.isEmpty() || plantilla.isEmpty()) {
+                // Si alguno de los archivos necesarios está vacío, muestra un mensaje de error para su correción manual.
                 System.out.println("Uno de los archivos necesarios está vacío. Verifique que el contenido de ListaClientes.csv y template.txt es el correcto.");
             } else {
-                vaciarCarpetaSalida();
-                combinarCorrespondencia(clientes, plantilla);
+                // Si ambos archivos se cargaron correctamente y no están vacíos, procede con la combinación de correspondencia.
+                vaciarCarpetaSalida(); //Primero vacía la carpeta de salida
+                combinarCorrespondencia(clientes, plantilla); // combina la correspondencia y genera nuevos archivos
                 System.out.println("Combinación de correspondencia completada.");
             }
         } catch (ArchivosFaltantesException e) {
+            // Captura una excepción si falta alguno de los archivos necesarios.
             System.out.println(e.getMessage());
             System.out.println("Por favor, verifique los archivos faltantes antes de volver a ejecutar el programa.");
         }
